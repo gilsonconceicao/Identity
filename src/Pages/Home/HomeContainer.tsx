22b58efefd0e40efea9d3a4f7149326fa2675193
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2023-08-07 22:57:19 -0300

    test clear data

--- src/Pages/Home/HomeContainer.tsx
+++ src/Pages/Home/HomeContainer.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text } from 'react-native'
+import { clearAsyncStorageData, getAsyncStorageData } from '../../Hooks/AsyncStorageIdentity'
+import { Button } from 'react-native-paper'
+import { useIdentity } from '../../Contexts/IdentiryContext'
 
 const HomeContainer = () => {
+  const { setIsLogged } = useIdentity();
+
+  const handleSignOut = () => {
+    clearAsyncStorageData("@identityUser");
+    setIsLogged(false);
+  }
+
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text>Hello world!</Text>
+      <Button
+        children="Clear data"
+        style={{width: 200}}
+        mode='contained'
+        onPress={handleSignOut}
+      />
     </View>
   )
 }
